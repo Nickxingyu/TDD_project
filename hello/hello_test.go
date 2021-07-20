557faed4c6a0aa1b3bce61d556a2083a80b4d486
XuXingyu <nick19960723@gmai..com>
2021-07-21 03:12:26 +0800

    Add a new test case.
    New test case
    
    1. When input string is empty string, Hello will return "Hello, word". not "Hello, "

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -5,9 +5,22 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := Hello("NicX")
-	want := "Hello, NicX"
-	if got != want {
-		t.Errorf("got: %s, want: %s", got, want)
+	assert_correct_message := func(t *testing.T, got string, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got: %s  want: %s", got, want)
+		}
 	}
+
+	t.Run("Say hello to people", func(t *testing.T) {
+		got := Hello("NicX")
+		want := "Hello, NicX"
+		assert_correct_message(t, got, want)
+	})
+
+	t.Run("Say hello word, if name is empty string.", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, word"
+		assert_correct_message(t, got, want)
+	})
 }
