43a73c39e575c13242d45c781a75baac1b3f9ed6
XuXingyu <nick19960723@gmai..com>
2021-07-21 03:32:51 +0800

    Add a new test case
    New test case:
    
    1. Hello prefix in Chinese
    
    	describe:
    		Let the Hello function can return different language hello prefix.
    		ex: Hello("許興宇", "Chinese") will return "哈摟, 許興宇"

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -13,14 +13,20 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("Say hello to people", func(t *testing.T) {
-		got := Hello("NicX")
+		got := Hello("NicX", "English")
 		want := "Hello, NicX"
 		assert_correct_message(t, got, want)
 	})
 
 	t.Run("Say hello word, if name is empty string.", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, word"
+		assert_correct_message(t, got, want)
+	})
+
+	t.Run("in Chinese", func(t *testing.T) {
+		got := Hello("許興宇", "Chinese")
+		want := "哈摟, 許興宇"
 		assert_correct_message(t, got, want)
 	})
 }
