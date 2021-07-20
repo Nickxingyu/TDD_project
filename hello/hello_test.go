0dfc3258a385c99a53c92b16dc047637e6161702
XuXingyu <nick19960723@gmai..com>
2021-07-21 02:56:27 +0800

    Let function "Hello" pass the test

---
+++ hello/hello_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	got := Hello("NicX")
+	want := "Hello, NicX"
+	if got != want {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
+}
