4ac66fd27c21f6f419d36e80eed7c8ba8a4c3d40
XuXingyu <nick19960723@gmai..com>
2021-07-21 03:36:50 +0800

    Let function "Hello" pass the test
    Test case:
    
    1. Hello prefix in Chinese
    
        describe:
    	Use switch to set the prefix text.

--- hello/hello.go
+++ hello/hello.go
@@ -1,12 +1,28 @@
 package main
 
 const english_hello_prefix = "Hello, "
+const chinese_hello_prefix = "哈摟, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
+	var hello_prefix string
+
+	//Check name is empty string or not
 	if name == "" {
 		name = "word"
 	}
-	return english_hello_prefix + name
+
+	//Determine hello_prefix language
+	switch language {
+	case "English":
+		hello_prefix = english_hello_prefix
+	case "Chinese":
+		hello_prefix = chinese_hello_prefix
+	default:
+		hello_prefix = english_hello_prefix
+		//Default language is English.
+	}
+
+	return hello_prefix + name
 }
 
 func main() {
