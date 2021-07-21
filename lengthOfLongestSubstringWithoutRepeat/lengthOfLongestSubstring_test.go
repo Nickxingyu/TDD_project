1a79c758b16e86e15188401b2b0642bfc3505d58
XuXingyu <nick19960723@gmai..com>
2021-07-22 05:22:49 +0800

    Write some test cases fot lengthOfLongestSubstring
    Describe for lengthOfLongestSubstring:
       Given a string s, find the length of the longest substring without repeating characters.
    
    Test case:
    1. Input is abcabcbb, output is 3
    2. Input is bbbbb, output is 1
    3. Input is pwwkew, output is 3
    4. Input is empty string, output is 0

---
+++ lengthOfLongestSubstringWithoutRepeat/lengthOfLongestSubstring_test.go
@@ -0,0 +1,50 @@
+package lengthOfLongestSubstring
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLengthOfLongestSubstring(t *testing.T) {
+	assert_correct_message := func(t *testing.T, result int, expect int) {
+		t.Helper()
+		if result != expect {
+			t.Errorf("result: %d, expect: %d", result, expect)
+		}
+	}
+	t.Run("Test case 1: Input is abcabcbb", func(t *testing.T) {
+		result := lengthOfLongestSubstring("abcabcbb")
+		expect := 3
+		assert_correct_message(t, result, expect)
+	})
+
+	t.Run("Test case 2: Input is bbbbb", func(t *testing.T) {
+		result := lengthOfLongestSubstring("bbbbb")
+		expect := 1
+		assert_correct_message(t, result, expect)
+	})
+
+	t.Run("Test case 3: Input is pwwkew", func(t *testing.T) {
+		result := lengthOfLongestSubstring("pwwkew")
+		expect := 3
+		assert_correct_message(t, result, expect)
+	})
+
+	t.Run("Test case 4: Input is empty", func(t *testing.T) {
+		result := lengthOfLongestSubstring("")
+		expect := 0
+		assert_correct_message(t, result, expect)
+	})
+}
+
+func BenchmarkLengthOfLongestSubstring(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		lengthOfLongestSubstring("abcabcbb")
+	}
+}
+
+func ExampleLengthOfLongestSubstring() {
+	result := lengthOfLongestSubstring("abcabcbb")
+	fmt.Println(result)
+	//Output: 3
+}
