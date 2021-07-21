b47e7e2b45025308605a376af110726394be273c
XuXingyu <nick19960723@gmai..com>
2021-07-21 19:31:06 +0800

    Write a test case
    Test case:
    1.  Add two integer and return the sum of these two integer

---
+++ adder/adder_test.go
@@ -0,0 +1,20 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expect := 4
+	if sum != expect {
+		t.Errorf("sum: %d, expect: %d", sum, expect)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	//Output: 6
+}
