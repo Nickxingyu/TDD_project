c1ace064974c09ecdc44b9b3793c57f50e7302f7
XuXingyu <nick19960723@gmai..com>
2021-07-21 20:03:39 +0800

    Write a test case
    Test case:
    1. Repeat a char a specific number of times

--- repeat/repeat_test.go
+++ repeat/repeat_test.go
@@ -5,15 +5,26 @@ import (
 )
 
 func TestRepeat(t *testing.T) {
-	result := Repeat("a")
-	expect := "aaaaa"
-	if result != expect {
-		t.Errorf("result: %s  expect: %s", result, expect)
+	assert_correct_message := func(t *testing.T, result string, expect string) {
+		t.Helper()
+		if result != expect {
+			t.Errorf("result: %s  expect: %s", result, expect)
+		}
 	}
+	t.Run("Repeat a char five time", func(t *testing.T) {
+		result := Repeat("a", 5)
+		expect := "aaaaa"
+		assert_correct_message(t, result, expect)
+	})
+	t.Run("Repeat a char a specific number of times", func(t *testing.T) {
+		result := Repeat("b", 10)
+		expect := "bbbbbbbbbb"
+		assert_correct_message(t, result, expect)
+	})
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
