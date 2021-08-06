5019073a541fc0a33a951acf44d13210126f5e42
XuXingyu <nick19960723@gmai..com>
2021-08-06 09:31:21 +0800

    Reconstruct the test, and use the interface type

--- shape/shape_test.go
+++ shape/shape_test.go
@@ -17,23 +17,26 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		got := rectangle.Area()
-		want := 100.0
+
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+
+		got := shape.Area()
 
 		if got != want {
 			t.Errorf("got = %v; want %v", got, want)
 		}
+	}
+
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		want := 100.0
+		checkArea(t, rectangle, want)
 	})
 
 	t.Run("circle", func(t *testing.T) {
 		circle := Circle{10.0}
-		got := circle.Area()
-		want := 314.16
-
-		if got != want {
-			t.Errorf("got = %v; want %v", got, want)
-		}
+		want := 314.1592653589793
+		checkArea(t, circle, want)
 	})
 }
