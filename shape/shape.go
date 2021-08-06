958817ef0d80a94158e4777a197dc0b7df0e4213
XuXingyu <nick19960723@gmai..com>
2021-08-06 09:30:04 +0800

    Add a new struct and method to pass the test
    Test case:
    1. circle area

--- shape/shape.go
+++ shape/shape.go
@@ -1,8 +1,16 @@
 package shape
 
+import (
+	"math"
+)
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+type Circle struct {
+	Radius float64
 }
 
 func (rectangle *Rectangle) Perimeter() float64 {
@@ -11,4 +19,12 @@ func (rectangle *Rectangle) Perimeter() float64 {
 
 func (rectangle *Rectangle) Area() float64 {
 	return rectangle.Width * rectangle.Height
+}
+
+func (circle *Circle) Perimeter() float64 {
+	return 2 * circle.Radius * math.Pi
+}
+
+func (circle *Circle) Area() float64 {
+	return math.Pi * circle.Radius * circle.Radius
 }
