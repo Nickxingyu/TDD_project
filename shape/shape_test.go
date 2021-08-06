c118be28198ccc836556a52452732f823fca21a8
XuXingyu <nick19960723@gmai..com>
2021-08-06 08:17:40 +0800

    Reconstruct the test case

--- shape/shape_test.go
+++ shape/shape_test.go
@@ -5,8 +5,9 @@ import (
 )
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
-	want := 55
+	input := Rectangle{10.0, 10.0}
+	got := Perimeter(input)
+	want := 40.0
 	if got != want {
 		t.Errorf("Perimeter(%v) = %v; want %v", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, got, want)
 	}
