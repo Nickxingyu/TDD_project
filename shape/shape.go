a4e2e307a39cb6df917450c002b08139b6c13a66
XuXingyu <nick19960723@gmai..com>
2021-08-06 10:05:59 +0800

    Add new struct to pass the test case
    Test case:
    1. {name: "Rectangle", shape: &Rectangle{Width: 10.0, Height: 10.0}, want: 100.0}
    2. {name: "Rectangle", shape: &Rectangle{Width: 15.0, Height: 10.0}, want: 150.0}
    3. {name: "Circle", shape: &Circle{Radius: 10.0}, want: 314.1592653589793}
    4. {name: "Circle", shape: &Circle{Radius: 100.0}, want: 31415.926535897932}
    5. {name: "Triangle", shape: &Triangle{Base: 12, Height: 6}, want: 36.0}

--- shape/shape.go
+++ shape/shape.go
@@ -17,6 +17,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func (rectangle *Rectangle) Perimeter() float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
@@ -31,4 +36,8 @@ func (circle *Circle) Perimeter() float64 {
 
 func (circle *Circle) Area() float64 {
 	return math.Pi * circle.Radius * circle.Radius
+}
+
+func (triangle *Triangle) Area() float64 {
+	return triangle.Base * triangle.Height * 0.5
 }
